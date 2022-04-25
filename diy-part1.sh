64d7d44bcbd57e7ae60489550e6c09c744bde337
aiyboy <59334928+aiyboy@users.noreply.github.com>
2022-04-25 20:27:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,4 @@
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git jell https://github.com/kenzok8/jell' feeds.conf.default
 #sed -i '$a src-git smpackage https://github.com/kenzok78/small-package' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok78/small-package' feeds.conf.default
